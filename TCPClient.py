c8e1209e376a56dff35063dcb30414fb628aaec7
Donal <osulld13@tcd.ie>
2015-10-14 17:23:24 +0100

    Initial Commit

---
+++ TCPClient.py
@@ -0,0 +1,42 @@
+import socket
+import sys
+import threadpool
+
+client_thread_pool = threadpool.ThreadPool(20)
+
+def connect_to_server_userin():
+    user_in = raw_input("input your message:\n>> ")
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ("localhost", 8000)
+    sock.connect(server_address)
+
+    sock.send(user_in)
+
+    data = sock.recv(1024)
+    print data
+
+    sock.close()
+
+#Sends test from the client
+def connect_to_server_auto():
+    user_in = "test message\n"
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ("localhost", 8000)
+    sock.connect(server_address)
+
+    sock.send(user_in)
+
+    data = sock.recv(1024)
+    print data
+
+    sock.close()
+
+if __name__ == '__main__':
+    #Main line for program
+    #Create 20 tasks that send messages to the server
+    for x in range(0, 200):
+        client_thread_pool.add_task(
+            connect_to_server_auto
+        )
---
+++ TCPServer.py
@@ -0,0 +1,42 @@
+import sys
+import socket
+import threadpool
+
+#global threadpool for server
+server_thread_pool = threadpool.ThreadPool(40)
+
+def create_server_socket():
+    #create socket  and initialise to localhost:8000
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ("localhost", 8000)
+
+    print "starting up on %s port %s\n" % server_address
+
+    #bind socket to server address and wait for incoming connections4
+    sock.bind(server_address)
+    sock.listen(1)
+
+    while True:
+        print "waiting for a connection\n"
+        #sock.accept returns a 2 element tuple
+        connection, client_address = sock.accept()
+        #Hand the client interaction off to a seperate thread
+        server_thread_pool.add_task(
+            start_client_interaction,
+            connection,
+            client_address
+        )
+
+
+def start_client_interaction(connection, client_address):
+    try:
+        data = connection.recv(16)
+        print "received message: %s" % data
+        print "sending back message"
+        connection.sendall("message received")
+
+    finally:
+        connection.close()
+
+if __name__ == '__main__':
+    create_server_socket()
